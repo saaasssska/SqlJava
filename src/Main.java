f04c92321663a1cb0d73a1292a865557a9e2ac2c
saaasssska <fedyev981@gmail.com>
2024-10-27 00:10:05 +0300

    Добавлено: создание таблиц, заполнение данными DataOperations() заготовки функций под добавление, удаление, изменение данных в таблицах

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
     public static Statement stmt;
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         Connection c = null;
@@ -13,6 +15,8 @@ public class Main {
             c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sql", "postgres", "password");
             stmt = c.createStatement();
             //CreateTables();
+            //CreateData();
+            DataOperations();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Error: " + e.getClass().getName() + ": " + e.getMessage());
@@ -27,23 +31,19 @@ public class Main {
     }
 
     public static void CreateTables() throws SQLException {
-        if (stmt == null) {
-            throw new SQLException("Statement not initialized.");
-        }
-
         String sqlCreateTableCompany = "CREATE TABLE company " +
                 "(id SERIAL PRIMARY KEY NOT NULL, " +
                 "name_company TEXT NOT NULL, " +
                 "formation_date DATE NOT NULL, " +
-                "inn BIGINT NOT NULL, " +
-                "status BOOLEAN NOT NULL)";
+                "inn BIGINT NOT NULL)";
         stmt.executeUpdate(sqlCreateTableCompany);
 
         String sqlCreateTableCar = "CREATE TABLE car " +
                 "(id SERIAL PRIMARY KEY NOT NULL, " +
                 "brand TEXT NOT NULL, " +
                 "model TEXT NOT NULL, " +
-                "vin BIGINT NOT NULL)";
+                "vin BIGINT NOT NULL, " +
+                "status BOOLEAN NOT NULL)";
         stmt.executeUpdate(sqlCreateTableCar);
 
         String sqlConnect1 = "ALTER TABLE car " +
@@ -83,19 +83,66 @@ public class Main {
         stmt.executeUpdate(sqlConnection3);
     }
 
-    public static void DataOperations() {
+    public static void CreateData() throws SQLException {
+        String sqlDataCompany = "INSERT INTO company (name_company, formation_date, inn) VALUES " +
+                "('BelkaCar', '2010-06-15', 123456789012), " +
+                "('CityDrive', '2015-03-20', 987654321098), " +
+                "('Delimobil', '2018-11-11', 112233445566)";
+        stmt.executeUpdate(sqlDataCompany);
 
+        String sqlDataCar = "INSERT INTO car (brand, model, vin, status, company_id) VALUES " +
+                "('Tesla', 'Model S', 11111111111111111, TRUE, 1), " +
+                "('Ford', 'Mondeo', 22222222222222222, TRUE, 2), " +
+                "('Toyota', 'Corolla', 33333333333333333, FALSE, 1), " +
+                "('Nissan', 'Leaf', 44444444444444444, TRUE, 3), " +
+                "('Nissan', 'Teana', 55555555555555555, TRUE, 3), " +
+                "('Haval', 'Jolion', 66666666666666666, TRUE, 1), " +
+                "('Chery', 'Tiggo 4 pro', 77777777777777777, FALSE, 3), " +
+                "('Kia', 'Rio', 88888888888888888, TRUE, 2), " +
+                "('Mazda', '6', 99999999999999999, FALSE, 2)";
+        stmt.executeUpdate(sqlDataCar);
+
+        String sqlDataUsers = "INSERT INTO users (first_name, last_name, num_driver_license) VALUES " +
+                "('Dmitry', 'Popov', 123456789), " +
+                "('Kirill', 'Ivanov', 987654321), " +
+                "('Arseniy', 'Egorov', 456789123), " +
+                "('Arsen', 'Volkov', 984394209)";
+        stmt.executeUpdate(sqlDataUsers);
+
+        String sqlDataLease = "INSERT INTO lease (start_date, end_date, users_id, car_id) VALUES " +
+                "('2024-01-01', '2024-01-15', 1, 1), " +
+                "('2024-02-01', '2024-02-15', 2, 2), " +
+                "('2024-03-01', '2024-03-15', 3, 4)";
+        stmt.executeUpdate(sqlDataLease);
+
+        String sqlDataPayments = "INSERT INTO payments (date_pay, amount_pay, lease_id) VALUES " +
+                "('2024-01-10', 800, 1), " +
+                "('2024-02-10', 900, 2), " +
+                "('2024-03-10', 750, 3)";
+        stmt.executeUpdate(sqlDataPayments);
     }
 
-    public static void CreateData() throws SQLException {
+    public static void DataOperations() throws SQLException {
+        while (true) {
+            System.out.println("Изменение данных базы данных");
+            System.out.println("1 - добавить данные");
+            System.out.println("2 - удалить удалить");
+            System.out.println("3 - изменить данные");
+            int num = scan.nextInt();
+            if (num != 1 && num != 2 && num != 3) break;
+            switch (num) {
+                case 1: AddData(); break;
+                case 2: DelData(); break;
+                case 3: ChangeData(); break;
+            }
+        }
+    }
+
+    public static void AddData() throws SQLException {
 
     }
 
     public static void DelData() throws SQLException {
-
-    }
-
-    public static void AddData() throws SQLException {
 
     }
 
